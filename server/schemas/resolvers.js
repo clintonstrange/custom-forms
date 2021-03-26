ddc7bbfda7cf7d42bd5696713cb3b31d82159c9e
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-25 20:10:37 -0500

    framework for resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,75 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Demographic, Screening } = require('../models');
+const { User, Demographics, Screening } = require('../models');
+const { signToken } = require('../utils/auth');
 
 //this file will be the engine for being able to add things like symptoms to the data base if you are an authenticated user (admin user)
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('thoughts')
+            .populate('friends');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+      users: async () => {
+        return User.find()
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .select('-__v -password')
+          .populate('friends')
+          .populate('thoughts');
+      },
+    },
+  
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      },
+
+      addFriend: async (parent, { friendId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { friends: friendId } },
+            { new: true }
+          ).populate('friends');
+  
+          return updatedUser;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      }
+    }
+  };
+  
+  module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,25 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    symptoms: [Symptom]
+  }
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addSymptom(symptomText: String!): Symptom
+
+  }
+`;
 
 module.exports = typeDefs;
