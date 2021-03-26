559a179c6ae70fb0f690a6d99f7f64cf9e85d298
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-25 20:55:52 -0500

    slight resolver edit, schema index module export setup

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,6 @@
 //use this file to export typeDefs && resolvers
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,71 +5,73 @@ const { signToken } = require('../utils/auth');
 //this file will be the engine for being able to add things like symptoms to the data base if you are an authenticated user (admin user)
 
 const resolvers = {
-    Query: {
-      me: async (parent, args, context) => {
-        if (context.user) {
-          const userData = await User.findOne({ _id: context.user._id })
-            .select('-__v -password')
-            .populate('thoughts')
-            .populate('friends');
-  
-          return userData;
-        }
-  
-        throw new AuthenticationError('Not logged in');
-      },
-      users: async () => {
-        return User.find()
-          .select('-__v -password')
-          .populate('thoughts')
-          .populate('friends');
-      },
-      user: async (parent, { username }) => {
-        return User.findOne({ username })
-          .select('-__v -password')
-          .populate('friends')
-          .populate('thoughts');
-      },
-    },
-  
-    Mutation: {
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
-  
-        return { token, user };
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-  
-        const token = signToken(user);
-        return { token, user };
-      },
+	Query: {
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = await User.findOne({ _id: context.user._id }).select(
+					'-__v -password'
+				);
+				// .populate('thoughts')
+				// .populate('friends');
 
-      addFriend: async (parent, { friendId }, context) => {
-        if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { friends: friendId } },
-            { new: true }
-          ).populate('friends');
-  
-          return updatedUser;
-        }
-  
-        throw new AuthenticationError('You need to be logged in!');
-      }
-    }
-  };
-  
-  module.exports = resolvers;
+				return userData;
+			}
+
+			throw new AuthenticationError('Not logged in');
+		},
+		users: async () => {
+			return User.find().select('-__v -password');
+			// .populate('thoughts')
+			// .populate('friends');
+		},
+		user: async (parent, { username }) => {
+			return User.findOne({ username }).select('-__v -password');
+			// .populate('friends')
+			// .populate('thoughts');
+		}
+	},
+
+	Mutation: {
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const token = signToken(user);
+			return { token, user };
+		},
+
+		// ! Does this go in a separate mutation since it's not directly related to the user? Or does it stay here because it's related to the form which is directly related to the user?
+		addSymptom: async (parent, { symptomId }, context) => {
+			if (context.symptom) {
+				const updatedSymptom = await updatedSymptom
+					.findOneAndUpdate(
+						{ _id: context.symptom._id },
+						{ $addToSet: { symptoms: symptomId } },
+						{ new: true }
+					)
+					.populate('symptoms');
+
+				return updatedSymptom;
+			}
+
+			throw new AuthenticationError('You need to be logged in!');
+		}
+	}
+};
+
+module.exports = resolvers;
