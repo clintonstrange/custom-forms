bb531e9c8f5ba1370b00742f0919a02e2939b257
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-26 13:54:45 -0500

    updated proctor model and resolver file

--- server/models/Proctor.js
+++ server/models/Proctor.js
@@ -1,18 +1,18 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-//create a schema for basic demographic information
-//first name
-//last name
-//phone number
 
 const proctor = new Schema({
 	// this would be a unique number associated with each screen...can we auto generate a 10 digit number that never gets repeated?
 	screenNum: {},
 	// this would be the name of the individual proctoring the screen
 	documentor: {},
-	// what are the credentials of the individual (EMT, Paramedic, RN, MD)
+	// what are the credentials of the individual (Civilian, EMT, Paramedic, RN, MD)
 	credentials: {},
-	// this should be a date/time field when the screen took place
-	createdAt: {}
+	// this should be a date/time field when the screen took place - this is a manual entry
+	dateTime: {},
+	// this should be a timestamp field when the screen took place - this should be automatically generated
+	createdAt:{}
+
+	// anything else that you can think of regardin the proctor you can add here...
 });
