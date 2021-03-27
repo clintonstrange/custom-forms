2405ab4d1a255334e9dbf5cef0c36f073e959fa2
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-03-27 11:26:39 -0500

    testing role validation

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useStoreContext } from "../utils/GlobalState";
+import Auth from "../utils/auth";
+// import { idbPromise } from "../utils/helpers"
 
 const Form = () => {
+  const [state, dispatch] = useStoreContext();
+
+//   useEffect(() => {
+//     async function getForms() {
+//       const cart = await idbPromise('forms', 'get');
+//       dispatch({ type: ADD_FORMS, forms: [...forms] });
+//     };
+
+//     if (!state.cart.length) {
+//       getCart();
+//     }
+//   }, [state.cart.length, dispatch]);
+
+  console.log(state);
   return (
     <div className="container">
-      <p>FORMS GO HERE</p>
+      <h2>Your Forms</h2>
+      {Auth.loggedIn() ? (
+        <div>
+          <p>Welcome <span>{state.role}</span></p>
+        </div>
+      ) : (
+        //       <div>
+        //         {state.role.admin ? (
+        //         <div>
+        //             <p>This is an Admin</p>
+        //         </div>
+
+        //     ) : (
+        //         <div>
+        //         {state.role.manager ? (
+        //         <div>
+        //             <p>This is an Manager</p>
+        //         </div>
+        //     )}) : (
+        //         <div>
+        //         {state.role.director ? (
+        //         <div>
+        //             <p>This is an Director</p>
+        //         </div>
+        //     ) : (
+        //       <span>(log in to access your forms!)</span>
+        //     )}
+        //   </div>
+        // </div>
+
+        <h3>
+          <span role="img" aria-label="warning">
+            ⚠️
+          </span>
+          Please Login!
+        </h3>
+      )}
     </div>
   );
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,18 +13,10 @@ const typeDefs = gql`
     user: User
   }
 
-<<<<<<< HEAD
-type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    addSymptom(symptomText: String!): 'Symptom'
-    submitForm()
-=======
   type Query {
     user: User
     users: [User]
   }
->>>>>>> 141d8675ee7f5dde1dfc10a43769f9adb6c418b9
 
   type Mutation {
     login(email: String!, password: String!): Auth
