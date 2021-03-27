f4d603cb1ca536d4a8d0bea70ab275c89fb3fea8
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-03-26 23:03:51 -0500

    updated models

--- server/models/Proctor.js
+++ server/models/Proctor.js
@@ -1,18 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, isValidObjectId } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 
-const proctor = new Schema({
-	// this would be a unique number associated with each screen...can we auto generate a 10 digit number that never gets repeated?
-	screenNum: {},
-	// this would be the name of the individual proctoring the screen
-	documentor: {},
-	// what are the credentials of the individual (Civilian, EMT, Paramedic, RN, MD)
-	credentials: {},
-	// this should be a date/time field when the screen took place - this is a manual entry
-	dateTime: {},
-	// this should be a timestamp field when the screen took place - this should be automatically generated
-	createdAt:{}
+const proctorSchema = new Schema(
+	{
+		// this would be a unique number associated with each screen...can we auto generate a 10 digit number that never gets repeated?
+		// JAKE--- I believe that ObjectId's auto increment with 12 digits automatically in Mongo, so references the Screening Model should work
+		screenNum: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Screening'
+			}
+		],
+		// this would be the name of the individual proctoring the screen
+		documentor: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		// what are the credentials of the individual (Civilian, EMT, Paramedic, RN, MD)
+		credentials: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		// JAKE--- I moved the dateTime and createdAt to the Screening Model. If we treat the Proctor like a User and the Screening like a comment/post
+		// it would make sense in my mind to attach it to the Screening Model i.e. the screening session.
+		// Then, the User Model would be just for the hospital/clinic/etc.
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		}
+	}
+);
 
-	// anything else that you can think of regardin the proctor you can add here...
+proctorSchema.virtual('screenNumCount').get(function() {
+	return this.screenNum.length;
 });
+
+const Proctor = model('Proctor', proctorSchema);
+
+module.exports = Proctor;
--- server/models/Screening.js
+++ server/models/Screening.js
@@ -1,17 +1,55 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+const dateFormat = require('../utils/dateFormat');
+
 
 //this file will be used for building the schema for the screening componenets
 
-const covidScreeningForm = new Schema({
-	proctor: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Proctor'
+const covidScreeningSchema = new Schema(
+	{
+		proctor: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		symptoms: {
+			type: String,
+			required: true,
+			maxlength: 280
+		},
+		contact: {
+			type: Boolean,
+			required: true
+		},
+		positiveTest: {
+			type: Boolean,
+			required: true
+		},
+		travel: {
+			type: String,
+			required: true
+		},
+		dateTime: {
+			type: Number,
+			required: true,
+			trim: true
+		},
+		// this should be a timestamp field when the screen took place - this should be automatically generated
+		createdAt:{
+			type: Date,
+			default: Date.now,
+			get: timestamp => dateFormat(timestamp)
 		}
-	],
-	symptoms: {},
-	contact: {},
-	positiveTest: {},
-	travel: {}
-});
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		id: false
+	}
+);
+
+const Screening = model('Screening', covidScreeningSchema);
+
+module.exports = Screening;
