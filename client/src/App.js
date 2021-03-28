1df9bd62a46a4edbe0f25be2106fbb5f49ea2852
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-27 20:45:39 -0500

    integrating materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,50 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { StoreProvider } from "./utils/GlobalState";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { StoreProvider } from './utils/GlobalState';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import NoMatch from "./pages/NoMatch";
-import Signup from "./pages/Signup";
-import Form from "./pages/Form";
+// setting up the applicaiton to use materialize
+import 'materialize-css/dist/css/materialize.min.css';
+
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import Signup from './pages/Signup';
+import Form from './pages/Form';
 
 const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-  uri: "/graphql",
+	request: operation => {
+		const token = localStorage.getItem('id_token');
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : ''
+			}
+		});
+	},
+	uri: '/graphql'
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <StoreProvider>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/form" component={Form} />
-            <Route component={NoMatch} />
-          </Switch>
-          </StoreProvider>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<div>
+					<StoreProvider>
+						<Nav />
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/login' component={Login} />
+							<Route exact path='/signup' component={Signup} />
+							<Route exact path='/form' component={Form} />
+							<Route component={NoMatch} />
+						</Switch>
+					</StoreProvider>
+				</div>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
