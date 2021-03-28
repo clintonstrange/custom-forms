74c70ebacd1fe5a31df3ad54be4a22e8a4d489cf
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-28 18:04:23 -0500

    login page styled

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,56 +1,54 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_ME } from "../../utils/queries";
+import React from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_ME } from '../../utils/queries';
 
 function Nav() {
-  const loggedIn = Auth.loggedIn();
-  const { data: userData } = useQuery(QUERY_ME);
+	const loggedIn = Auth.loggedIn();
+	const { data: userData } = useQuery(QUERY_ME);
 
-  function showNavigation() {
-    if (loggedIn && userData) {
-      return (
-        <div className="container">
-          <ul className="flex-row">
-            <li className="mx-1">
-              <a href="/" onClick={() => Auth.logout()}>
-                Logout
-              </a>
-            </li>
-            <li>DATA RENDERING LINK HERE</li>
-            {userData.me.role === "admin" ? (
-              <div>
-                <li className="mx-1">
-                  <a href="/signup">Create Account</a>
-                </li>
-              </div>
-            ) : null}
-          </ul>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </div>
-      );
-    }
-  }
+	function showNavigation() {
+		if (loggedIn && userData) {
+			return (
+				<div className='nav-wrapper blue darken-1'>
+					<a href='/' class='brand-logo left'>
+						Whatever Works 2.0
+					</a>
+					<ul className='flex-row right'>
+						<li className='mx-1'>
+							<a href='/' onClick={() => Auth.logout()}>
+								Logout
+							</a>
+						</li>
+						<li className='margin-right'>DATA RENDERING LINK HERE</li>
+						{userData.me.role === 'admin' ? (
+							<div>
+								<li className='mx-1'>
+									<a href='/signup'>Create Account</a>
+								</li>
+							</div>
+						) : null}
+					</ul>
+				</div>
+			);
+		} else {
+			return (
+				<div className='nav-wrapper blue darken-1'>
+					<a href='/' class='brand-logo left margin-left'>
+						Whatever Works 2.0
+					</a>
+					<ul className='flex-row right'>
+						<li className='mx-1'>
+							<Link to='/login'>Login</Link>
+						</li>
+					</ul>
+				</div>
+			);
+		}
+	}
 
-  return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">Whatever Works 2.0</Link>
-      </h1>
-
-      <nav>{showNavigation()}</nav>
-    </header>
-  );
+	return <nav>{showNavigation()}</nav>;
 }
 
 export default Nav;
