4805f1f6b94f8626038e5fcf72c4f2246b684a0e
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-31 12:42:34 -0500

    footer added, submit button styled, re-arraged order of screening

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,48 +1,54 @@
-import React from "react";
-import Auth from "../utils/auth";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_ME } from "../utils/queries";
-import Form from "../components/Form";
+import React from 'react';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_ME } from '../utils/queries';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
 // import { idbPromise } from "../utils/helpers"
 
 const Landing = () => {
-  const loggedIn = Auth.loggedIn();
-  const { data: userData } = useQuery(QUERY_ME);
+	const loggedIn = Auth.loggedIn();
+	const { data: userData } = useQuery(QUERY_ME);
 
-  return (
-    <div className="container">
-      {loggedIn && userData ? (
-        <div>
-          <p>
-            Welcome <span>{userData?.me?.username}</span>
-          </p>
-          <p>
-            Authorization: <span>{userData?.me?.role.toUpperCase()}</span>
-          </p>
-          <div>
-            {userData?.me?.role === "admin" ? (
-              <div>
-                <Form />
-              </div>
-            ) : userData?.me?.role === "read/write" ? (
-              <div>
-                <Form />
-              </div>
-            ) : (
-              <div>
-                <p>
-                  if logged in and user does not have admin or read/write
-                  access, default to read only access
-                </p>
-              </div>
-            )}
-          </div>
-        </div>
-      ) : (
-        <h3>Please Login to Access your Forms!</h3>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<div className='form-container'>
+				{loggedIn && userData ? (
+					<div>
+						<p>
+							Welcome <span>{userData?.me?.username}</span>
+						</p>
+						<p>
+							Authorization: <span>{userData?.me?.role.toUpperCase()}</span>
+						</p>
+						<div>
+							{userData?.me?.role === 'admin' ? (
+								<div>
+									<Form />
+								</div>
+							) : userData?.me?.role === 'read/write' ? (
+								<div>
+									<Form />
+								</div>
+							) : (
+								<div>
+									<p>
+										if logged in and user does not have admin or read/write
+										access, default to read only access
+									</p>
+								</div>
+							)}
+						</div>
+					</div>
+				) : (
+					<h3>Please Login to Access your Forms!</h3>
+				)}
+			</div>
+			<div className='form-footer'>
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default Landing;
