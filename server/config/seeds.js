88078e0c45034e9bf8e9fee49b69e1d5283cda4a
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-03-31 22:55:14 -0500

    updated seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,270 +1,51 @@
 const db = require("./connection");
 const { User, Screenings, Control } = require("../models");
 db.once("open", async () => {
-  //   await Control.deleteMany();
+  await Control.deleteMany();
 
-  //   await Control.create({
-  //     documentor: "Perry Cox",
-  //     screenNum: [],
-  //     credentials: "MD",
-  //   });
+  await Control.create({
+    documentor: "Perry Cox",
+    screenNum: [],
+    credentials: "MD",
+  });
 
-  //   await Control.create({
-  //     documentor: "Carla Turk",
-  //     screenNum: [],
-  //     credentials: "RN",
-  //   });
+  await Control.create({
+    documentor: "Carla Turk",
+    screenNum: [],
+    credentials: "RN",
+  });
 
-  //   await Control.create({
-  //     documentor: "John Dorian",
-  //     screenNum: [],
-  //     credentials: "Paramedic",
-  //   });
+  await Control.create({
+    documentor: "John Dorian",
+    screenNum: [],
+    credentials: "Paramedic",
+  });
 
-  //   await Control.create({
-  //     documentor: "Chris Turk",
-  //     screenNum: [],
-  //     credentials: "EMT",
-  //   });
+  await Control.create({
+    documentor: "Chris Turk",
+    screenNum: [],
+    credentials: "EMT",
+  });
 
-  //   await Control.create({
-  //     documentor: "Glen Matthews",
-  //     screenNum: [],
-  //     credentials: "Civilian",
-  //   });
+  await Control.create({
+    documentor: "Glen Matthews",
+    screenNum: [],
+    credentials: "Civilian",
+  });
 
   console.log(" ========= CONTROL SEEDED ========= ");
+  await Screenings.deleteMany();
+
   // await Screenings.deleteMany();
 
   // await Screenings.create({
-  //   control: "6063c897dd32db9339298553",
+  //   control: "6063bf26595bdfd2515b8c72",
   //   symptoms: "noSymptom",
   //   contact: "no",
   //   positiveTest: "no",
   //   travel: "no",
   //   screenDate: "March 1, 2021",
   // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298553",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 1, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298553",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 1, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298554",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 1, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298554",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 2, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298554",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 2, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298554",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 2, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298555",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 2, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298556",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 2, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298556",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 3, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298556",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 3, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298556",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 3, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298556",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 3, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298557",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 3, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298557",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298557",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298557",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063c897dd32db9339298557",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c75",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c75",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-  // await Screenings.create({
-  //   control: "6063bf26595bdfd2515b8c76",
-  //   symptoms: "noSymptom",
-  //   contact: "no",
-  //   positiveTest: "no",
-  //   travel: "no",
-  //   screenDate: "March 4, 2021",
-  // });
-
-  console.log(" ========= CONTROL SEEDED ========= ");
-
-  await Screenings.deleteMany();
-
-  await Screenings.create({
-    control: "6063bf26595bdfd2515b8c72",
-    symptoms: "noSymptom",
-    contact: "no",
-    positiveTest: "no",
-    travel: "no",
-    screenDate: "March 1, 2021",
-  });
 
   //   console.log(" ========= screenings seeded ========= ");
   await User.deleteMany();
